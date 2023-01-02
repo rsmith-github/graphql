e60f521126e53ae632257453d23782863e49199d
rsmith-github <rsmith.git@gmail.com>
2023-01-01 22:19:46 -0500

    feat(grid): update grid layout

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import LineChart from './LineChart';
 const client = new QueryClient();
 const endpoint = "https://learn.01founders.co/api/graphql-engine/v1/graphql";
 
-
 function App() {
+
   return (
     <QueryClientProvider client={client}>
       <h1>GraphQL</h1>
@@ -18,7 +18,7 @@ function App() {
         <Profile />
         <Ratio />
         <Projects />
-
+        <Graphs />
       </div>
     </QueryClientProvider>
   );
@@ -32,6 +32,11 @@ export function Profile() {
           id
           login
         }
+        transaction(where: { userId: { _eq: 1954 }, path: {_like: "%image-upload"}, type: {_eq: "level"}}){
+          userId
+          type
+          amount
+        }
   }
   `;
 
@@ -44,12 +49,13 @@ export function Profile() {
 
 
   return (
-    <div className='page' id='profile'>
+    <div className='page top-left' id='profile'>
       {data.user.map((user) => (
         <h2 id='profile-header'>ID: {user.id}, Login: {user.login}</h2>
-
       ))}
-      <p>An amazing fellow</p>
+      {data.transaction.map((transaction) => (
+        <h2 id='login'>{transaction.type}: {transaction.amount}</h2>
+      ))}
     </div>
   );
 }
@@ -57,7 +63,7 @@ export function Profile() {
 export function Ratio() {
 
   return (
-    <div className='page' id='ratio'>
+    <div className='page top-right' id='ratio'>
       <h2>Some Ratio</h2>
     </div>
   );
@@ -88,20 +94,60 @@ export function Projects() {
 
 
   return (
-    <div className='page' id='projects'>
-      <h2>Projects</h2>
-      <LineChart 
+    <div className='page bottom' id='projects'>
+      <h2>XP in Piscine-Go</h2>
+      {/* <LineChart 
           // object.name and amount
           xyPoints={data}
-      />
-      
-      {/* {data.transaction.map((project, i) => (
-        <p key={project.id}>
-          {project.object.name}
+      /> */}
+
+      {data.transaction.map((project) => (
+        <>
+          <p key={project.id}>
+            <span>Challenge Name: </span>{project.object.name}
+          </p>
+          <p>
+            <span>XP: </span>{project.amount}
+          </p>
           <br></br>
-          {project.amount}
-        </p>
-      ))} */}
+        </>
+      ))}
+
+    </div >
+  );
+}
+export function Graphs() {
+
+  const PROJECTS_QUERY = `
+  {
+      transaction(where: { userId: { _eq: 1954 }, path: {_like: "%piscine-go/quest-%"}}){
+          type
+          amount
+          objectId
+          createdAt
+          object {
+          id
+            name
+          }
+      }
+  }
+  `;
+
+  const { data, isLoading, error } = useQuery("projects", () => {
+    return request(endpoint, PROJECTS_QUERY);
+  });
+
+  if (isLoading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>;
+
+
+  return (
+    <div className='page' id='graphs'>
+      <h2>XP in Piscine-Go (graph)</h2>
+      <LineChart
+        // object.name and amount
+        xyPoints={data}
+      />
 
     </div >
   );
